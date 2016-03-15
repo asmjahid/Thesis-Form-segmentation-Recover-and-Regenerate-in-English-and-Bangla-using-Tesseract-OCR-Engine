37b594394864759f121c78ff5b0856c8011a1390
tanvir hasan <tanvir002700@gmail.com>
2016-03-15 14:31:48 +0600

    new traindata

--- Tesseract Train/Training/init.sh
+++ Tesseract Train/Training/init.sh
@@ -1,5 +1,22 @@
 text2image --list_available_fonts --fonts_dir=.
 rm font_properties
+#<fontname> <italic> <bold> <fixed> <serif> <fraktur>
+echo "Arial 0 0 0 0 0" >> font_properties
+echo "Arial Bold 0 1 0 0 0" >> font_properties
+echo "Arial Bold Italic 1 1 0 0 0" >> font_properties
+echo "Arial Heavy 0 0 0 0 0" >> font_properties
+echo "Arial Italic 1 0 0 0 0" >> font_properties
+echo "Calibri 0 0 0 0 0" >> font_properties
+echo "Calibri Bold 0 1 0 0 0" >> font_properties
+echo "Calibri Bold Italic 1 1 0 0 0" >> font_properties
+echo "Calibri Italic 1 0 0 0 0" >> font_properties
+echo "Calibri Light 0 0 1 0 0" >> font_properties
+echo "Calibri Light Italic 1 0 1 0 0" >> font_properties
+echo "Courier 0 0 0 0 0" >> font_properties
+echo "Courier New 0 0 0 0 0" >> font_properties
+echo "Courier New Bold 0 1 0 0 0" >> font_properties
+echo "Courier New Bold Italic 1 1 0 0 0" >> font_properties
+echo "Courier New Italic 1 0 0 0 0" >> font_properties
 echo "Times new Roman, 0 0 0 1 0" >> font_properties
 echo "Times New Roman, Bold 0 1 0 1 0" >> font_properties
 echo "Times New Roman, Bold Italic 1 1 0 1 0" >> font_properties
--- Tesseract Train/Training/makebox.sh
+++ Tesseract Train/Training/makebox.sh
@@ -1,13 +1,33 @@
-text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp0 --font='Times New Roman,' --fonts_dir=.
+lang="eng"
+rm errorlog
+FontName=("Arial" "Arial Bold" "Arial Bold" "Arial Bold Italic" "Arial Heavy" "Arial Italic" "Calibri" "Calibri Bold" "Calibri Bold Italic" "Calibri Italic" "Calibri Light" "Calibri Light Italic" "Courier New" "Courier New Bold" "Courier New Bold Italic" "Courier New Italic" "Times new Roman," "Times New Roman, Bold" "Times New Roman, Bold Italic" "Times New Roman, Italic")
+
+OutputBase=("Arial" "Arial" "Arial" "Arial" "Arial" "Arial" "Calibri" "Calibri" "Calibri" "Calibri" "Calibri" "Calibri" "Courier" "Courier" "Courier" "Courier" "TimesNewRoman" "TimesNewRoman" "TimesNewRoman" "TimesNewRoman")
 
 
-text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp1 --font='Times New Roman, Bold' --fonts_dir=.
+for i in "${!FontName[@]}"; do 
+	fontname=${FontName[$i]}
+	BASE="$lang.${OutputBase[$i]}.exp$i"
+	text2image --text=training.txt --outputbase=$BASE --font="$fontname" --fonts_dir=.
+	if [ $? -eq 0 ]
+	then 
+		echo "Successfully execute: $fontname" >> errorlog
+	else
+		echo "Failed to build: $fontname" >> errorlog
+	fi
+done
+
+cat errorlog
+#text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp0 --font='Times New Roman,' --fonts_dir=.
 
 
-text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp2 --font='Times New Roman, Bold Italic' --fonts_dir=.
+#text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp1 --font='Times New Roman, Bold' --fonts_dir=.
 
 
-text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp3 --font='Times New Roman, Italic' --fonts_dir=.
+#text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp2 --font='Times New Roman, Bold Italic' --fonts_dir=.
+
+
+#text2image --text=training.txt --outputbase=eng.TimesNewRoman.exp3 --font='Times New Roman, Italic' --fonts_dir=.
 
 
 
